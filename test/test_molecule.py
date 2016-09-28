8b42a818c2fb48600b4039e67077e5d676719d9d
Ted Cook <teodoro.cook@gmail.com>
2016-09-28 12:20:08 -0600

    Moved tests to pytest

---
+++ test/test_molecule.py
@@ -0,0 +1,29 @@
+import pytest
+import sys
+import os
+import shutil
+from subprocess import call
+from cookiecutter.main import cookiecutter
+
+
+playbook_test_command = "molecule test"
+playbook_test_success = 0
+
+
+@pytest.mark.parametrize('role_name', ['tree'])
+def test_role_name(role_name):
+    last_dir = os.path.curdir
+    project_name="ansible-role-{0}".format(role_name)
+    test_dir = project_name 
+    try:
+        shutil.rmtree(test_dir, ignore_errors=True)
+        cookiecutter(
+                '.', 
+                no_input=True, 
+                overwrite_if_exists=True,
+                extra_context={'role_name': role_name, 'project_name': project_name})
+        os.chdir(project_name)
+        assert call(playbook_test_command.split()) == playbook_test_success
+    finally:
+        os.chdir(last_dir)
+        shutil.rmtree(test_dir, ignore_errors=True)
