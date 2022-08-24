51eea16b7e03d36a907303346c4e4451220928b8
Ted Cook <teodoro.cook@gmail.com>
2022-08-23 22:27:10 -0600

    Move dependency management to poetry

--- test/test_molecule.py
+++ test/test_molecule.py
@@ -5,10 +5,9 @@ from subprocess import call
 from cookiecutter.main import cookiecutter
 
 
-playbook_setup_commands = ['pip install -r https://raw.githubusercontent.com/nephelaiio/ansible-role-requirements/molecule3/requirements.txt']
+playbook_setup_commands = ['poetry install']
 playbook_setup_success = 0
-playbook_test_image = 'geerlingguy/docker-ubuntu2004-ansible:latest'
-playbook_test_command = "molecule test"
+playbook_test_command = "poetry run molecule test"
 playbook_test_success = 0
 
 
@@ -29,7 +28,6 @@ def test_role_name(role_name):
             assert call(command.split()) == playbook_setup_success
         os.chdir(test_dir)
         custom_env = os.environ.copy()
-        custom_env['MOLECULE_DOCKER_IMAGE'] = playbook_test_image
         assert call(playbook_test_command.split(), env=custom_env) == \
             playbook_test_success
     finally:
--- {{cookiecutter.project_name}}/molecule/default/tests/test_default.py
+++
@@ -1,2 +0,0 @@
-def test_command(host):
-    assert host.command('{{ cookiecutter.role_name }} --version').rc == 0
