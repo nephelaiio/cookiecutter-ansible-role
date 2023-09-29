45ac4f094ae473eb4ede5d88a79123178c1425ec
Ted Cook <teodoro.cook@gmail.com>
2023-09-28 21:40:32 -0600

    Refactor tests to use makefile targets

--- test/test_molecule.py
+++ test/test_molecule.py
@@ -4,11 +4,10 @@ import shutil
 from subprocess import call
 from cookiecutter.main import cookiecutter
 
-
-playbook_setup_commands = ['poetry install']
-playbook_setup_success = 0
-playbook_test_command = "poetry run molecule test"
-playbook_test_success = 0
+test_commands = [
+    "make lint",
+    "make test"
+]
 
 
 @pytest.mark.parametrize('role_name', ['tree'])
@@ -16,6 +15,7 @@ def test_role_name(role_name):
     last_dir = os.path.curdir
     project_name = "nephelaiio.{0}".format(role_name)
     test_dir = project_name
+    succeeds = lambda x: x == 0
     try:
         shutil.rmtree(test_dir, ignore_errors=True)
         cookiecutter(
@@ -24,12 +24,9 @@ def test_role_name(role_name):
                 overwrite_if_exists=True,
                 extra_context={'role_name': role_name}
         )
-        for command in playbook_setup_commands:
-            assert call(command.split()) == playbook_setup_success
         os.chdir(test_dir)
-        custom_env = os.environ.copy()
-        assert call(playbook_test_command.split(), env=custom_env) == \
-            playbook_test_success
+        for command in test_commands:
+            assert succeeds(call(command.split()))
     finally:
         os.chdir(last_dir)
         shutil.rmtree(test_dir, ignore_errors=True)
