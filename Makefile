eb7a7aa08dc57e847b81e17b953f066e09df7f18
Ted Cook <teodoro.cook@gmail.com>
2023-09-28 18:22:34 -0600

    Fix task interpolation errors

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
 .PHONY: install test
 
+USER_NAME ?= nephelaiio
+ROLE_NAME ?= tree
+
+clean:
+	rm -rf "${USER_NAME}.${ROLE_NAME}"
+
 install:
 	@type poetry >/dev/null || pip3 install poetry
 	@poetry install
 
 test: install
+	USER_NAME=${USER_NAME} \
+	ROLE_NAME=${ROLE_NAME} \
 	poetry run pytest -s
--- test/test_molecule.py
+++ test/test_molecule.py
@@ -5,16 +5,19 @@ from subprocess import call
 from cookiecutter.main import cookiecutter
 
 
-playbook_setup_commands = ['poetry install']
-playbook_setup_success = 0
-playbook_test_command = "poetry run molecule test"
-playbook_test_success = 0
+role_name = os.environ["ROLE_NAME"]
+user_name = os.environ["USER_NAME"]
+setup_commands = ['poetry install']
+setup_success = 0
+test_commands = ["poetry run molecule test"]
+test_success = 0
 
 
-@pytest.mark.parametrize('role_name', ['tree'])
-def test_role_name(role_name):
+@pytest.mark.parametrize('user', [user_name])
+@pytest.mark.parametrize( 'role', [role_name])
+def test_role(user, role):
     last_dir = os.path.curdir
-    project_name = "nephelaiio.{0}".format(role_name)
+    project_name = "nephelaiio.{0}".format(role)
     test_dir = project_name
     try:
         shutil.rmtree(test_dir, ignore_errors=True)
@@ -22,14 +25,13 @@ def test_role_name(role_name):
                 '.',
                 no_input=True,
                 overwrite_if_exists=True,
-                extra_context={'role_name': role_name}
+                extra_context={'role_name': role, 'galaxy_user': user}
         )
-        for command in playbook_setup_commands:
-            assert call(command.split()) == playbook_setup_success
+        for command in setup_commands:
+            assert call(command.split()) == setup_success
         os.chdir(test_dir)
-        custom_env = os.environ.copy()
-        assert call(playbook_test_command.split(), env=custom_env) == \
-            playbook_test_success
+        for command in test_commands:
+            assert call(command.split()) == test_success
     finally:
         os.chdir(last_dir)
         shutil.rmtree(test_dir, ignore_errors=True)
