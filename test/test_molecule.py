b8087bff5eaf00ef7c2557403f7bc8b5721fc506
Teodoro Cook <teodoro.cook@gmail.com>
2025-02-17 15:49:45 -0600

    Move dependency management to devbox (#82)

--- test/test_molecule.py
+++ test/test_molecule.py
@@ -1,33 +1,36 @@
-import pytest
 import os
 import shutil
 from subprocess import call
+
+import pytest
 from cookiecutter.main import cookiecutter
 
-test_commands = [
-    "make lint",
-    "make test"
-]
+test_commands = ["make lint", "make test"]
 
 
-@pytest.mark.parametrize('role_name', ['tree'])
+# Helper function to check if a command succeeded
+def succeeds(x):
+    return x == 0
+
+
+@pytest.mark.parametrize("role_name", ["tree"])
+# Test whether the role_name is valid
 def test_role_name(tmpdir, role_name):
     last_dir = os.path.curdir
     project_name = "nephelaiio.{0}".format(role_name)
     test_basename = project_name
     test_dir = os.path.join(tmpdir, test_basename)
-    succeeds = lambda x: x == 0
     try:
         cookiecutter(
-                '.',
-                no_input=True,
-                overwrite_if_exists=True,
-                extra_context={'role_name': role_name}
+            ".",
+            no_input=True,
+            overwrite_if_exists=True,
+            extra_context={"role_name": role_name},
         )
         shutil.move(test_basename, test_dir)
         os.chdir(test_dir)
-        for command in test_commands:
-            assert succeeds(call(command.split()))
+        assert succeeds(call("devbox run make lint".split()))
+        assert succeeds(call("devbox run make test".split()))
     finally:
         os.chdir(last_dir)
         shutil.rmtree(test_dir, ignore_errors=True)
